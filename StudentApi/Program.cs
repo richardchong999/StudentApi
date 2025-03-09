27e2b5b5d6b2a2c8b894b50ff74fb91184d02298
Kong Ho <kong.ho@gov.ab.ca>
2025-03-09 15:01:55 -0600

    testing pushing to GitHub.  DT 2025/03/09@3:01pm

--- StudentApi/Program.cs
+++ StudentApi/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// testing push and pull 2025/03/09 2:56pm
+
 // Register the StudentContext with the connection string
 builder.Services.AddDbContext<StudentContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
